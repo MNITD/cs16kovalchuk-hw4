2bc4848a477fa2bbcc80500fb5aedf4c26a14aa0
MNITD <bogdanleblank@ukr.net>
2016-12-10 23:07:53 +0200

    initial commit bug with external library org.hamcrest.Matchers.containsInAnyOrder

---
+++ src/test/java/ua/edu/ucu/autocomplete/PrefixMatchesITTest.java
@@ -0,0 +1,49 @@
+
+package test.java.ua.edu.ucu.autocomplete;
+
+import main.java.ua.edu.ucu.autocomplete.PrefixMatches;
+import main.java.ua.edu.ucu.tries.RWayTrie;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ * @author Andrii_Rodionov
+ */
+public class PrefixMatchesITTest {
+
+    private PrefixMatches pm;
+
+    @Before
+    public void init() {
+        pm = new PrefixMatches(new RWayTrie());
+        pm.load("abc", "abce", "abcd", "abcde", "abcdef");
+    }
+
+    @Test
+    public void testWordsWithPrefix_String() {
+        String pref = "ab";
+
+        Iterable<String> result = pm.wordsWithPrefix(pref);
+
+        String[] expResult = {"abc", "abce", "abcd", "abcde", "abcdef"};
+
+        assertThat(result, containsInAnyOrder(expResult));
+    }
+
+    @Test
+    public void testWordsWithPrefix_String_and_K() {
+        String pref = "abc";
+        int k = 3;
+
+        Iterable<String> result = pm.wordsWithPrefix(pref, k);
+
+        String[] expResult = {"abc", "abce", "abcd", "abcde"};
+
+        assertThat(result, containsInAnyOrder(expResult));
+    }
+
+}
