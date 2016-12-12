c05a5d26f7e2a6df796e07b86a95175d63c8c8c5
MNITD <bogdanleblank@ukr.net>
2016-12-12 02:21:47 +0200

    added several method to RWayTrie

--- src/main/java/ua/edu/ucu/tries/RWayTrie.java
+++ src/main/java/ua/edu/ucu/tries/RWayTrie.java
@@ -1,11 +1,45 @@
 package main.java.ua.edu.ucu.tries;
 
-public class RWayTrie implements Trie {
+public class RWayTrie<T> implements Trie {
+    private final int R;
+    private Node root;
+
+    public RWayTrie(int rway) {
+        R = rway;
+        root = new Node(rway);
+    }
 
     @Override
     public void add(Tuple t) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Node node = root;
+
+        if (node == null) {
+            node = new Node(t.term.charAt(0), R);
+        }
+        for (int i = 1; i < t.term.length(); i++) {
+            Node temp = node.getNext(t.term.charAt(i));
+            if (temp == null) {
+                temp = new Node(t.term.charAt(i), R);
+                node.setNext(t.term.charAt(i), temp);
+            }
+            node = temp;
+        }
+        node.setValue(t.weight);
+//        root.setNext(t.term.charAt(0), addHelper(root.getNext(t.term.charAt(0)), t.term, 0, t.weight));
     }
+
+//    private Node addHelper(Node node, String str, int i, Object value) {
+//        if (node == null) {
+//            node = new Node(str.charAt(i), R);
+//        }
+//
+//        if (i + 1 == str.length()) {
+//            node.setValue(value);
+//            return node;
+//        }
+//        node.setNext(str.charAt(i + 1), addHelper(node.getNext(str.charAt(i + 1)), str, i + 1, value));
+//        return node;
+//    }
 
     @Override
     public boolean contains(String word) {
@@ -14,7 +48,37 @@ public class RWayTrie implements Trie {
 
     @Override
     public boolean delete(String word) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        boolean r = deleteHelper(root.getNext(word.charAt(0)), word, 0);
+        if(r){
+            root.getNext(word.charAt(0)).decreaseChildNumber();
+            if(root.getNext(word.charAt(0)).getChildNumber() == 0){
+                root.setNext(word.charAt(0), null);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean deleteHelper(Node node, String s, int i) {
+        if (i < s.length() && node == null) {
+            return false;
+        } else if (i == s.length() - 1) {
+            if (node.getChildNumber() == 0) {
+                return true;
+            }
+            return false;
+        } else {
+            boolean t = deleteHelper(node.getNext(s.charAt(i + 1)), s, i + 1);
+
+            if(t){
+                node.getNext(s.charAt(i + 1)).decreaseChildNumber();
+                if(node.getNext(s.charAt(i + 1)).getChildNumber() == 0){
+                    node.setNext(s.charAt(i + 1), null);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
     @Override
@@ -29,7 +93,7 @@ public class RWayTrie implements Trie {
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }
